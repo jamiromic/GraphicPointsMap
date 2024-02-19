8fab15bebfea1ab47ec6817af7076090572aedae
jamiromic <jamiromic@gmail.com>
2024-02-19 10:20:55 +0100

    progress

--- graphic_points.py
+++ graphic_points.py
@@ -8,7 +8,7 @@ x2, y2 = x0, y1  # Angolo in alto a sinistra
 x3, y3 = x1, y0  # Angolo in basso a destra
 
 # Crea il grafico
-plt.figure(figsize=(8, 6))
+plt.figure(figsize=(10, 6))
 
 # Disegna il rettangolo
 plt.plot([x0, x2, x1, x3, x0], [y0, y2, y1, y3, y0], color='blue')
